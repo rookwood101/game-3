352ce135fd7bf27ffcf86fb4d7340ce297a91b53
Unknown <t3d.y34t3s@gmail.com>
2018-05-13 23:03:04 +0100

    Uncle Ghost Now Has a Lust for Spanking Bad Ghost Children

--- Assets/UncleGhostMovement.cs
+++ Assets/UncleGhostMovement.cs
@@ -1,14 +1,17 @@
-﻿using System.Collections;
+﻿using Pathfinding;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UncleGhostMovement : MonoBehaviour {
 
     private Pathfinding.AIDestinationSetter uncleGhostTarget;
+    private IAstarAI ai;
     private GameObject naughtyBoy;
 
     // Use this for initialization
     void Start () {
+        ai = GetComponent<IAstarAI>();
         naughtyBoy = GameObject.Find("Ghost");
         uncleGhostTarget = GetComponent<Pathfinding.AIDestinationSetter>();
         uncleGhostTarget.target = naughtyBoy.transform;
@@ -16,8 +19,14 @@ public class UncleGhostMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		if (ai.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-2, 2, 1);
+        } else if (ai.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(2, 2, 1);
+        }
+    }
 
 
 }
