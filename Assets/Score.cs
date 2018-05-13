51b7571582206d2c866d7c557f708266cd475a71
Unknown <t3d.y34t3s@gmail.com>
2018-05-13 16:07:16 +0100

    Added Score Code to Ghost
    However no display implementation yet

---
+++ Assets/Score.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    private int score;
+
+    private void Awake()
+    {
+        EventManager.AddListener(EventTypes.Runaway, AddPoints);
+        EventManager.AddListener(EventTypes.NPCExit, AddPoints);
+    }
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void AddPoints(object npcScared)
+    {
+        score++;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "NPC")
+        {
+            score--;
+        }
+    }
+
+}
