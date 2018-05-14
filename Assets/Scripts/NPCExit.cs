69a282bbd5fcac18f08e5dfab0fc859d524cd7d5
rookwood101 <alexandersadler1@gmail.com>
2018-05-14 21:12:19 +0100

    Add close door sound

--- Assets/Scripts/NPCExit.cs
+++ Assets/Scripts/NPCExit.cs
@@ -2,22 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPCExit : MonoBehaviour {
+public class NPCExit : MonoBehaviour
+{
 
     private bool exited;
+    private AudioSource[] shouts;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        shouts = GetComponents<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     // Check if player leaves
-    private void OnTriggerEnter2D(Collider2D collision)
+    private async void OnTriggerEnter2D(Collider2D collision)
     {
         if (exited)
             return;
@@ -25,9 +30,11 @@ public class NPCExit : MonoBehaviour {
         GameObject touchingObject = collision.gameObject;
 
         //Check if NPC is touching the door and npc is trying to leave
-        if(touchingObject.name == "FrontDoor" && GetComponent<Pathfinding.AIDestinationSetter>().target == touchingObject.transform)
+        if (touchingObject.name == "FrontDoor" && GetComponent<Pathfinding.AIDestinationSetter>().target == touchingObject.transform)
         {
             exited = true;
+            shouts[3].Play();
+            await Wait.ForIEnumerator(new WaitForSeconds(0.5f));
             EventManager.TriggerEvent(EventTypes.NPCExit, gameObject);
 
         }
